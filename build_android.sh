cada352570d3ee37cd33d56546dbcddf1e4d1779
peter-s <peter_future@outlook.com>
2016-07-15 18:37:28 +0800

    add build script

---
+++ build_android.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ -z "$ANDROID_NDK" ]; then
+	echo "You must define ANDROID_NDK before starting."
+	echo "They must point to your NDK directories.\n"
+	exit 1
+fi
+
+# Detect OS
+OS=`uname`
+HOST_ARCH=`uname -m`
+export CCACHE=; type ccache >/dev/null 2>&1 && export CCACHE=ccache
+if [ $OS == 'Linux' ]; then
+	export HOST_SYSTEM=linux-$HOST_ARCH
+elif [ $OS == 'Darwin' ]; then
+	export HOST_SYSTEM=darwin-$HOST_ARCH
+fi
+
+
+SOURCE=`pwd`
+PREFIX=$SOURCE/build/
+
+SYSROOT=$ANDROID_SYSROOT
+CROSS_PREFIX=arm-linux-androideabi-
+
+./configure  --prefix=$PREFIX \
+  --host=arm-linux \
+  --with-sysroot=${SYSROOT} \
+  --disable-shared \
+  --enable-static \
+  --with-pic=no \
+  CC="${CROSS_PREFIX}gcc --sysroot=${SYSROOT}" \
+  CXX="${CROSS_PREFIX}g++ --sysroot=${SYSROOT}" \
+  RANLIB="${CROSS_PREFIX}ranlib" \
+  AR="${CROSS_PREFIX}ar" \
+  AR_FLAGS=rcu \
+  STRIP="${CROSS_PREFIX}strip" \
+  NM="${CROSS_PREFIX}nm" \
+  CFLAGS="-O3  --sysroot=${SYSROOT}" \
+  CXXFLAGS="-O3 --sysroot=${SYSROOT}" \
+  || exit 1
+
+make clean
+make -j4 install || exit 1
+
